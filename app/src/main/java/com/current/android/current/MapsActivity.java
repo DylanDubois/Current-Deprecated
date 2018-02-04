2a9cebc1fcc9a21730b8de08df1bc87536ff0c62
Dylan Dubois <dubois.a.dylan@gmail.com>
2018-02-04 14:29:33 -0600

    Tracks user location

--- app/src/main/java/com/current/android/current/MapsActivity.java
+++ app/src/main/java/com/current/android/current/MapsActivity.java
@@ -1,5 +1,14 @@
 package com.current.android.current;
 
+import android.*;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,6 +29,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private Button eventsButton;
     private Button postButton;
     private Button settingsButton;
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+
+    // Change to network on mobile
+    private String LOCATION_PROVIDER = LocationManager.GPS_PROVIDER;
+
+    private final int REQUEST_CODE = 123;
+    private final long MIN_TIME = 500;
+    private final float MIN_DISTANCE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,11 +89,74 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-//        LatLng lsuMarker = new LatLng(30.4133, -91.18);
-//        LatLngBounds LSU = new LatLngBounds(new LatLng(30.15, -91.35), new LatLng(30.55, -91.1));
+        LatLng lsuMarker = new LatLng(30.4133, -91.18);
+        LatLngBounds LSU = new LatLngBounds(new LatLng(30.15, -91.35), new LatLng(30.55, -91.1));
 //        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(LSU,0));
-//        mMap.addMarker(new MarkerOptions().position(lsuMarker).title("Marker at LSU"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.addMarker(new MarkerOptions().position(lsuMarker).title("Marker at LSU"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(lsuMarker));
 
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d("Current", "resume() Called");
+        getCurrentUserLocation();
+    }
+
+    private void getCurrentUserLocation() {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.d("Current", "onLocationChanged()");
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Log.d("Current", "providerDisabled()");
+            }
+        };
+
+        // TODO: Change Fine to Coarse when using physical device
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this, new String[]
+                    {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+        locationManager.requestLocationUpdates(LOCATION_PROVIDER, MIN_TIME, MIN_DISTANCE, locationListener);
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_CODE){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Log.d("Current", "onReqPermission = granted");
+            }
+            else{
+                Log.d("Current", "Permission denied ;(");
+            }
+        }
     }
 }
